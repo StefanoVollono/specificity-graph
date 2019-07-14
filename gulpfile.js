a64a9bb1b1f92ec801ffc0ffc3cd3e1df8c8bb78
Stefano Vollono <stefanovollono@gmail.com>
2019-07-14 16:33:50 +0200

    fix lodash template pollution

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 var gulp          = require('gulp');
 var del           = require('del');
-var runSequence   = require('run-sequence');
 
 var useref        = require('gulp-useref');
 var gulpif        = require('gulp-if');
@@ -11,26 +10,21 @@ var minifyCss     = require('gulp-clean-css');
 var htmlmin       = require('gulp-htmlmin');
 
 
-gulp.task('dist', function (callback) {
-  runSequence('clean:github-page','useref', callback)
-});
-
-
 // Build dist
-gulp.task('useref', function(){
-  return gulp.src(['app/index.html'])
+function optimizeAssets() {
+  return gulp
+    .src(['app/index.html'])
     .pipe(useref())
     .pipe(gulpif('*.html', htmlmin({collapseWhitespace: true, minifyJS:true, removeComments:true})))
     .pipe(gulpif('*.js', uglify()))
     .pipe(gulpif('*.css', minifyCss()))
     .pipe(gulp.dest('github-page'))
-});
-
-
-
-
+};
 
 // clean dist folder
-gulp.task('clean:github-page', function() {
-  return del.sync('github-page');
-});
+function clean() {
+  del('github-page');
+}
+
+const dist = gulp.series(optimizeAssets);
+exports.dist  = dist;
